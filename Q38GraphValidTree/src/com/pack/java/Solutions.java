16aab3729e067dd5361ded37c7552b593148cd6c
mansiiyengar <mansi.mi11@gmail.com>
2015-11-29 02:21:42 -0800

    i

---
+++ Q38GraphValidTree/src/com/pack/java/Solutions.java
@@ -0,0 +1,35 @@
+package com.pack.java;
+
+import java.util.Arrays;
+
+//get the middle no such that the numbers higher than it are not greater
+
+public class Solutions { 
+	
+    public boolean validTree(int n, int[][] edges) {
+        // initialize n nums
+        int[] nums = new int[n];
+        Arrays.fill(nums, -1);
+
+        // perform union find
+        for (int i = 0; i < edges.length; i++) {
+            int x = find(nums, edges[i][0]);
+            int y = find(nums, edges[i][1]);
+
+            // if two vertices happen to be in the same set
+            // then there's a cycle
+            if (x == y) return false;
+
+            // union
+            nums[x] = y;
+        }
+
+        return edges.length == n - 1;
+    }
+
+    int find(int nums[], int i) {
+        if (nums[i] == -1) return i;
+        return find(nums, nums[i]);
+    }
+}
+
