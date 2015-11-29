4739cd35fa8b4c22e94ac834eefbe14f2dea9890
mansiiyengar <mansi.mi11@gmail.com>
2015-11-28 23:02:24 -0800

    Initial

---
+++ Q33DigitsToEnglish/src/com/pack/java/DigitsToEnglish.java
@@ -0,0 +1,33 @@
+/*32. Find the kth largest element in an unsorted array. 
+ Note that it is the kth largest element in the sorted order,
+  not the kth distinct element.
+For example,
+Given [3,2,1,5,6,4] and k = 2, return 5.
+Note: 
+You may assume k is always valid, 1 <= k <= array's length.
+Answer: The smart approach for this problem is to use the 
+selection algorithm (based on the partition method - the same one as used in quicksort).
+
+*/
+package com.pack.java;
+
+import java.util.Arrays;
+
+public class DigitsToEnglish {
+
+
+	
+public static void main(String[] args) {
+	Solutions sol= new Solutions();
+	int A[]={3,2,1,5,6,4};
+
+	System.out.println(sol.numberToWords(2679));
+	
+       
+    }
+
+}	
+	
+	
+
+
